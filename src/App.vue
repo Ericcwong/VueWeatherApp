88a9d303a46691f9764d7fc718982e6152da0380
Eric Wong <ericwong12@live.com>
2020-08-20 13:03:58 -0600

    Moved api call to app.vue Working on passing data down to other components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,98 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <CurrentDay />
+  <input type="text" v-model="state.query" @keyup.enter="weatherSearch" class="field" />
+  <p>{{state.query}}</p>
+  <CurrentDay :currentDayData="state.weather" />
+  <div class="currentDay">
+    <!-- WeatherWrap is to load conditionally, v-if is saying if state.weather.main is undefined dont render whats below -->
+    <div class="weatherWrap" v-if="typeof state.weather.main != 'undefined'">
+      <div class="locationBox">
+        <div class="location">{{state.weather.name}}</div>
+        {{state.weather}}
+        <div class="date">{{getCurrentDate()}}</div>
+      </div>
+      <div class="weatherBox">
+        <div class="temp">{{Math.round(state.weather.main.temp)}}°F</div>
+        <div class="weather">{{state.weather.weather.description}}</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import CurrentDay from "./components/CurrentDay.vue";
-
+import { reactive } from "vue";
 export default {
   name: "App",
-  components: {
-    CurrentDay,
+  setup() {
+    // State variables that stores all the data
+    const state = reactive({
+      apiKey: "c0d97a019ea859a14447316fcc3b3bce",
+      urlBase: "https://api.openweathermap.org/data/2.5/weather?q=",
+      query: "",
+      weather: {},
+    });
+    // API call
+    function weatherSearch(e) {
+      if (e.key == "Enter") {
+        fetch(
+          `${state.urlBase}${state.query}&units=imperial&appid=${state.apiKey}`
+        )
+          .then((res) => {
+            console.log(res);
+            return res.json();
+          })
+          .then((results) => {
+            state.weather = results;
+            console.log(state.weather);
+          });
+        // }
+      }
+    }
+    //Setting current dates to each day
+    function getCurrentDate() {
+      let d = new Date();
+      let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+      let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+      return `${day}, ${month}, ${date}, ${year}`;
+    }
+
+    return {
+      state,
+      weatherSearch,
+      getCurrentDate,
+    };
   },
 };
 </script>
+
+<style scoped>
+.weatherWrap {
+  border: 1px solid black;
+}
+</style>
--- src/modules/weather.js
+++ src/modules/weather.js
@@ -1,15 +1,31 @@
-import {
-    ref
-} from "vue";
+// import {
+//     reactive,
+//     toRefs
+// } from "vue";
 
-export default function useWeather() {
-    const weather = ref(null);
+// export default function useWeather() {
 
-    const load = async () => {
-        try {
-            const weatherResponse = await fetch("")
-        } catch (error) {
-            error.value = error;
-        }
-    }
-}
+//     const state = reactive({
+//         apiKey: "c0d97a019ea859a14447316fcc3b3bce",
+//         urlBase: "https://api.openweathermap.org/data/2.5/weather?q=",
+//         query: "",
+//         weather: {},
+//         loaded: false
+//     });
+
+//     const load = async () => {
+//         if (!state.loaded) {
+//             try {
+//                 const weatherResponse = await fetch("https://api.openweathermap.org/data/2.5/weather?q=denver&appid=c0d97a019ea859a14447316fcc3b3bce")
+//                 state.weather = await weatherResponse.json();
+//             } catch (error) {
+//                 state.error = error;
+//             }
+//         }
+//     }
+
+//     return {
+//         ...toRefs(state),
+//         load
+//     }
+// }
